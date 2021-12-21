4b3ceddc157ca0a2b4a8b40c5bf2255acf389c1c
Princess <adwoaasante88@gmail.com>
2021-12-21 05:39:11 +0000

    chapter 2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,7 +3,7 @@ import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class ProductService {
-  private _ablumUrl = '../assets/album.json'
+  private _albumUrl = "../assets/album.json";
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
