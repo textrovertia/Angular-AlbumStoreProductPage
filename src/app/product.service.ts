d571e45eb7b6b66e98614f90347c614642721b28
Princess <adwoaasante88@gmail.com>
2021-12-21 17:38:57 +0000

    done

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ export class ProductService {
   private _albumUrl = "../assets/album.json";
   constructor(private _http: Http) { }
 
-  getAlbum(id: Number){
+  getAlbum(id: number){
     return this._http.get(this._albumUrl).map((response)=>{
       response.json()
     })
