df333109ee43eb5feab20d5fb66cf4d066ba9d4c
Princess <adwoaasante88@gmail.com>
2021-12-21 05:26:39 +0000

    chapter 2

---
+++ src/app/product.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+
+}
