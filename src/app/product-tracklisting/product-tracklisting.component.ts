9a808e62c3921d1db4cfe5b6421a2d72bee135cd
Princess <adwoaasante88@gmail.com>
2021-12-22 02:33:54 +0000

    done

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Album } from 'app/album';
+import { ProductService } from 'app/product.service';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+  albumInfo: Album;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
