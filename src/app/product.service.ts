123db26ebd66ccb5e636499e5decee01134f26a4
Princess <adwoaasante88@gmail.com>
2021-12-22 03:06:46 +0000

    done

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -18,9 +18,9 @@ export class ProductService {
     <Album>response.json())
   }
 
-  getProducts(){
+  getProducts(): Observable<Product[]>{
     return this._http.get(this._productsUrl).map((response) => 
-    response.json())
+    <Product[]>response.json())
     };
   
 }
